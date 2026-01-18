7aba67f6e1a4dbead35f946f948bb82d2c8011db
Subit <bhoisubit@gmail.com>
2026-01-18 20:57:22 +0530

    added levels

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,61 @@ const guessInput = document.getElementById("guessInput");
 const guessBtn = document.getElementById("guessBtn");
 const message = document.querySelector(".message");
 const restartBtn = document.getElementById("restartBtn");
+const difficultyLevel = document.getElementById("difficultyLevel");
+const attemptLevel = document.getElementById("attemptLevel");
+const instructions = document.querySelector(".instructions");
+const attempts = document.querySelector(".attempts");
+const difficultyLevelSettings = {
+    easy: { max: 10 },
+    medium: { max: 50 },
+    hard: { max: 100 }
+}
+const attemptLevelSettings = {
+    easy: { attempts: 5 },
+    medium: { attempts: 3 },
+    hard: { attempts: 1 }
+}
 
-let secretNumber = Math.floor(Math.random() * 10) + 1;
-let attemptsLeft = 3;
+let currentMax = "";
+let currentAttempts = "";
+let secretNumber = Math.floor(Math.random() * currentMax) + 1;
+attemptLevel.disabled = true;
+guessInput.disabled = true;
+guessBtn.disabled = true;
 
-let attempt = document.querySelector(".attempt");
+difficultyLevel.addEventListener("change", function () {
+    const selectedDifficulty = difficultyLevel.value;
+    const difficultySettings = difficultyLevelSettings[selectedDifficulty];
+    currentMax = difficultySettings.max;
+    secretNumber = Math.floor(Math.random() * currentMax) + 1;
+    message.textContent = `Guess the number between 1 & ${currentMax}`;
+    instructions.textContent = `Guess the number between 1 & ${currentMax}`;
+    guessInput.placeholder = `1 - ${currentMax}`;
+    guessInput.value = "";
+    guessInput.disabled = true;
+    guessBtn.disabled = true;
+    attemptLevel.disabled = false;
+    attemptLevel.focus();
+    resetGame();
+});
+
+attemptLevel.addEventListener("change", function () {
+    const selectedAttempt = attemptLevel.value;
+    const attemptSettings = attemptLevelSettings[selectedAttempt];
+    currentAttempts = attemptSettings.attempts;
+    attempts.textContent = `Attempt left: ${currentAttempts}`;
+    guessInput.value = "";
+    guessInput.disabled = false;
+    guessBtn.disabled = false;
+    guessInput.focus();
+    resetGame();
+});
 
 guessBtn.addEventListener("click", function () {
     const userGuess = Number(guessInput.value);
 
-    if (!userGuess || userGuess < 1 || userGuess > 10) {
-        message.textContent = "❌ Please enter a number between 1 & 10";
+    if (!userGuess || userGuess < 1 || userGuess > currentMax) {
+        message.textContent = `❌ Please enter a number between 1 & ${currentMax}`;
         return;
     }
 
@@ -30,8 +74,8 @@ guessBtn.addEventListener("click", function () {
         return;
     }
 
-    attemptsLeft--;
-    attempt.textContent = attemptsLeft;
+    currentAttempts--;
+    attempts.textContent = `Attempt left: ${currentAttempts}`;
 
     if (userGuess > secretNumber) {
         message.textContent = "👆 Too High! Guess again";
@@ -46,23 +90,27 @@ guessBtn.addEventListener("click", function () {
     }, 400);
 
 
-    if (attemptsLeft === 0) {
+    if (currentAttempts === 0) {
         message.textContent = `🚫 Game Over! The number was ${secretNumber}`;
         guessBtn.disabled = true;
         guessInput.disabled = true;
     }
 });
 
-restartBtn.addEventListener("click", function () {
-    secretNumber = Math.floor(Math.random() * 10) + 1;
-    attemptsLeft = 3;
-
-    attempt.textContent = attemptsLeft;
-    message.textContent = "Guess a number between 1 & 10";
+function resetGame() {
+    selectedAttempt = attemptLevel.value;
+    attemptSettings = attemptLevelSettings[selectedAttempt];
+    currentAttempts = attemptSettings.attempts;
+    attempts.textContent = `Attempt left: ${currentAttempts}`;
+    secretNumber = Math.floor(Math.random() * currentMax) + 1;
     guessInput.value = "";
-    guessBtn.disabled = false;
     guessInput.disabled = false;
+    guessBtn.disabled = false;
     guessInput.focus();
+}
+
+restartBtn.addEventListener("click", function () {
+    resetGame();
 });
 
 guessInput.addEventListener("keydown", function (event) {
@@ -73,6 +121,6 @@ guessInput.addEventListener("keydown", function (event) {
 
 document.addEventListener("keydown", function (event) {
     if (event.key === "Escape") {
-        restartBtn.click();
+        resetGame();
     }
 });
